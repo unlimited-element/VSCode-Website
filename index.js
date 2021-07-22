8e8ab267c76108f1d95a9f093a160ea094921ff6
unlimited-element <73407996+unlimited-element@users.noreply.github.com>
2021-07-21 21:14:23 -0700

    Completed unique button and theme toggle
    Refactored code to limit lines of unecessary code. Refactored toggler to include just a toggler

--- index.js
+++ index.js
@@ -4,13 +4,6 @@ const swap = document.querySelector('.btn');
 
 const swapper = () => {
     document.body.classList.toggle('dark-theme')
-    var className = document.body.className;
-    if (className == "light-theme") {
-        this.textContent = "Dark";
-    } else {
-        this.textContent = "Light";
-    }
-    console.log('current class name: ' + className);
 }
 
 swap.addEventListener('click', swapper);
